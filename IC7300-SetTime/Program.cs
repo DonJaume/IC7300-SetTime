46f2abb721eb9508380392b3eb45bddfd951c61d
Jaume <xispi1@hotmail.com>
2020-10-03 00:30:12 +0200

    Iniciando proyecto

---
+++ IC7300-SetTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC7300_SetTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
